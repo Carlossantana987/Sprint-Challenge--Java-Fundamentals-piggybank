0cdbabe0a7d26bf6e40097e3d3987a873902e0c4
Carlossantana987 <carlossantana987@outlook.com>
2019-07-12 13:09:58 -0400

    changed approach to project Money classes finished

---
+++ src/piggyBankApp/Dimes.java
@@ -0,0 +1,25 @@
+package piggyBankApp;
+
+public class Dimes extends TheMoney
+{
+    private int dimes;
+    
+    public Dimes(int dimes){
+        this.dimes = dimes;
+    }
+
+    public Dimes(){
+        this.dimes = this.dimes + 1;
+    }
+
+
+    public double getValue(){
+        return dimes * 0.10;
+    }
+
+    public String getName(){
+        String plural = dimes > 1 ? "s":"";
+        return this.dimes + " Dime" + plural;
+    }
+
+}
--- src/piggyBankApp/TheMoney.java
+++ src/piggyBankApp/TheMoney.java
@@ -1,3 +1,13 @@
 package piggyBankApp;
 
-public 
+public abstract class TheMoney
+{
+	public abstract String getName();
+	
+    public abstract double getValue();
+
+    @Override
+    public String toString(){
+        return getName();
+    }
+}
